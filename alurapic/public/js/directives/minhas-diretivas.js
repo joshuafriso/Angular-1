21b65405aad32d532bcaa7aed28a78492abcab88
jflima <joshua.lima@ccee.org.br>
2018-03-16 07:27:45 -0300

    Bônus

--- alurapic/public/js/directives/minhas-diretivas.js
+++ alurapic/public/js/directives/minhas-diretivas.js
@@ -56,4 +56,16 @@ angular.module('minhasDiretivas', []).directive('meuPainel', function(){
 				});
 			};
         return ddo;
+}).directive('meusTitulos', function() {
+        var ddo = {};
+        ddo.restrict = 'E';
+        ddo.template = '<ul><li ng-repeat="titulo in titulos">{{titulo}}</li></ul>';
+				ddo.controller = function($scope, recursoFoto){
+					recursoFoto.query(function(fotos) {
+					      $scope.titulos = fotos.map(function(foto) {
+					       	return foto.titulo;
+					       });    
+					});
+				};
+        return ddo;
 });
